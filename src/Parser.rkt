03b3f83d4e2c2a76b22ab0c44df247796cf9930a
cekicEfe <efecekic0@gmail.com>
2025-10-26 21:14:36 +0300

    newer version

--- src/Parser.rkt
+++ src/Parser.rkt
@@ -23,9 +23,12 @@
       (input [() '()]
              [(input statement_list) (append $1 $2)])
 
-      (statement [(exp)])
+      (statement_list [(statement_list statement) (append $1 $2)]
+                      [(statement) $1])
+      (statement [(exp_statement) $1])
 
-      (statement_list [(statement_list statement) (append $1 $2)])      
+      (exp_statement [(END_STATEMENT) '()]
+                     [(exp END_STATEMENT) $1])
 
       (exp [(exp ADD term)      (list $1 'ADD $3)]
            [(exp SUBTRACT term) (list $1 'SUBTRACT $3)]
